247fb71f000d47d9304e99feb759eaa2f03b1513
AlsuSha <Alsou307@gmail.com>
2021-05-22 17:24:16 +0300

    TestProgectCreated

---
+++ SeleniumTest/TestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class TestBase
+    {
+        
+        private IWebDriver driver;
+        private string baseURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://software-testing.ru/lms/my/";
+            driver.Navigate().GoToUrl(baseURL);
+            driver.Quit();
+        }
+    }
+}

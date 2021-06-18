3423d7e1553fa7d311571df98bac2d0b0f54888a
Alsou <salsou96@mail.ru>
2021-06-18 22:04:58 +0300

    Add files via upload

---
+++ HW19/Tests/TestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace HW
+{
+    [TestClass]
+    public class TestBase
+    {
+        public Application app;
+
+        [TestInitialize]
+        public void start()
+        {
+            app = new Application();
+        }
+
+        [TestCleanup]
+        public void stop()
+        {
+            app.Quit();
+            app = null;
+        }
+    }
+}
